3ff72edf8288af3a75186cf68b983af8e423c97f
Sherman Tay <65003339+ShermsRL@users.noreply.github.com>
2024-08-01 13:28:50 +0800

    add navbar component (#2)
    * add navbar component
    
    * change layout of navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,45 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "bootstrap/dist/css/bootstrap.css";
+
+function AppNavBar() {
+  return (
+    <Container>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container className="justify-content-center">
+          <Navbar.Brand href="#home">NavigateSG</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="m-auto flex-grow-1 justify-content-evenly">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#traffic">Traffic</Nav.Link>
+
+              <NavDropdown title="MRT Map" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#map/3.1">East-West</NavDropdown.Item>
+                <NavDropdown.Item href="#map/3.2">North-South</NavDropdown.Item>
+                <NavDropdown.Item href="#map/3.3">North-East</NavDropdown.Item>
+                <NavDropdown.Item href="#map/3.4">Circle</NavDropdown.Item>
+                <NavDropdown.Item href="#map/3.5">Downtown</NavDropdown.Item>
+                <NavDropdown.Item href="#map/3.6">
+                  Thomson-East Coast
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#map/3.7">Full Map</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#help">Help</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
+  );
+}
+
+export default AppNavBar;
