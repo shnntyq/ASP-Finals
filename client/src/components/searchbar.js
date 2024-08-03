d9291aa42f3ebfe6ce0a91d23f1463c6361f6aad
Sherman Tay <65003339+ShermsRL@users.noreply.github.com>
2024-08-03 13:40:19 +0800

    connect search bar to backend (#6)
    * connect search bar to backend
    
    * refactor backend

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -3,17 +3,41 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import React, { useState } from "react";
+import axios from "axios";
 
 function SearchBar() {
+  const url = "http://localhost:3001/";
+  const [data, setData] = useState({
+    curr_location: "",
+    dest_location: "",
+  });
+
+  function submit(e) {
+    e.preventDefault();
+    console.log(data);
+    axios.post(url, { loc: data });
+  }
+
+  function handle(e) {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+    console.log(newData);
+  }
+
   return (
     <Container className="mt-4">
-      <Form inline>
+      <Form onSubmit={(e) => submit(e)}>
         <Row>
           <Col>
             <Form.Control
               type="text"
               placeholder="Current Location"
               className=" mr-sm-2"
+              id="curr_location"
+              value={data.curr_location}
+              onChange={(e) => handle(e)}
             />
           </Col>
           <Col>
@@ -21,6 +45,9 @@ function SearchBar() {
               type="text"
               placeholder="Destination"
               className=" mr-sm-2"
+              id="dest_location"
+              value={data.dest_location}
+              onChange={(e) => handle(e)}
             />
           </Col>
           <Col xs="auto">
