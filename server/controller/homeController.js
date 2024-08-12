60ab69b50056e9f947411676d45c78e97cbee22b
Sherman <shermantaymk@gmail.com>
2024-08-12 14:42:22 +0800

    add function to get location details

--- server/controller/homeController.js
+++ server/controller/homeController.js
@@ -1,6 +1,39 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function getLocationDetails(location) {
+  const data = JSON.stringify(false);
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open(
+    "GET",
+    `https://www.onemap.gov.sg/api/common/elastic/search?searchVal=${location}&returnGeom=Y&getAddrDetails=Y`,
+    true
+  );
+
+  xhr.send(data);
+
+  return data;
+}
+
 exports.home = async (req, res, next) => {
   try {
-    console.log(req.body);
+    let current_loc = req.body.loc.curr_location;
+    let destination_loc = req.body.loc.dest_location;
+
+    const current_loc_details = getLocationDetails(current_loc);
+    const destination_loc_details = getLocationDetails(destination_loc);
+
+    console.log(current_loc_details);
+    console.log(destination_loc_details);
+
+    console.log(current_loc);
+    console.log(destination_loc);
   } catch (err) {
     next(err);
   }
