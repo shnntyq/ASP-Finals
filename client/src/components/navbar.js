7ebd6caed86078686b3101fbc757f13ec7d750b7
ZhiJun01 <147889637+ZhiJun01@users.noreply.github.com>
2024-08-28 21:45:06 +0800

    Update navbar.js
    made it look cleaner

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -8,60 +8,58 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function AppNavBar() {
   return (
-    <Container>
-      <Navbar bg="dark" variant="dark">
-        <Container className="justify-content-center">
-          <Navbar.Brand as={Link} to="/">
-            NavigateSG
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="m-auto flex-grow-1 justify-content-evenly">
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>{" "}
-              {/* Link to HomeComponent */}
-              <Nav.Link as={Link} to="/traffic">
-                Traffic
-              </Nav.Link>{" "}
-              {/* Link to TrafficComponent */}
-              <NavDropdown title="MRT Map" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/map/east-west">
-                  East-West
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/map/north-south">
-                  North-South
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/map/north-east">
-                  North-East
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/map/circle">
-                  Circle
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/map/downtown">
-                  Downtown
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/map/thomson-east-coast">
-                  Thomson-East Coast
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="/map/full">
-                  Full Map
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link as={Link} to="/help">
-                Help
-              </Nav.Link>{" "}
-              {/* Link to HelpComponent */}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Container>
+      <div style={{width: "100%"}}>
+        <Navbar bg="dark" variant="dark" expand= "lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              NavigateSG
+            </Navbar.Brand>
+          </Container>
+
+          <Container>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className=" flex-grow-1 justify-content-evenly">
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/traffic">
+                  Traffic
+                </Nav.Link>
+
+                <NavDropdown title="MRT Map" id="basic-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="/map/east-west">
+                    East-West
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/map/north-south">
+                    North-South
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/map/north-east">
+                    North-East
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/map/circle">
+                    Circle
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/map/downtown">
+                    Downtown
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/map/thomson-east-coast">
+                    Thomson-East Coast
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to="/map/full">
+                    Full Map
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link as={Link} to="/help">
+                  Help
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+
+        </Navbar>
+      </div>
   );
 }
 
