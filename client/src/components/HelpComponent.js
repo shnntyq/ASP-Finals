445ab15123b3598602769d7b0afffefd76ce10cf
ZhiJun01 <147889637+ZhiJun01@users.noreply.github.com>
2024-08-31 01:46:15 +0800

    Update HelpComponent.js

--- client/src/components/HelpComponent.js
+++ client/src/components/HelpComponent.js
@@ -3,6 +3,31 @@ import Container from "react-bootstrap/Container";
 
 function HelpComponent() {
   return (
+
+//        <Container>
+//          <div style={{ padding: "20px" }}>
+//            <h2>Help & Support</h2>
+//            <p>
+//              Welcome to the Help section! Here you can find answers to common
+//              questions and guides on how to use the website.
+//            </p>
+//            <ul>
+//              <li>
+//                <strong>How to Use the MRT Map:</strong> Select a map from the
+//                dropdown menu to view different MRT lines.
+//              </li>
+//              <li>
+//                <strong>Traffic Information:</strong> Navigate to the Traffic
+//                section to see real-time traffic updates.
+//              </li>
+//              <li>
+//                <strong>Contact Support:</strong> If you need further assistance,
+//                please contact our support team via the contact form.
+//              </li>
+//            </ul>
+//          </div>
+//        </Container>                  
+    
        <Container style={{ maxWidth: '100%', padding: "0"}}>
 
       {/* TOP PORTION */}
