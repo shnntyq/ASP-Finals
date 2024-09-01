8df32843135227c240b7e06645c6e2f2085d399a
Sherman <shermantaymk@gmail.com>
2024-09-01 15:54:40 +0800

    create fe for traffic update

--- server/routes/trafficRoute.js
+++ server/routes/trafficRoute.js
@@ -1,16 +1,16 @@
-const dataService = require("../services/dataService");
+const dataService  = require("../services/dataService");
 const express = require("express");
 const router = express.Router();
 
 // returns bus arrival info of input bus stop code
-router.get("/getTrafficIncidents", function (req, res) {
-  dataService.getTrafficIncidents(function (err, TrafficIncidents) {
-    if (err) {
-      res.status(401).send("unable to get traffic status");
-    } else {
-      res.status(200).send(TrafficIncidents);
-    }
+router.get('/getTrafficIncidents', function (req, res) {
+    dataService.getTrafficIncidents(function (err, TrafficIncidents) {
+        if (err) {
+            res.status(401).send("unable to get traffic status");
+        } else {
+            res.status(200).send(TrafficIncidents);
+        }
+    });
   });
-});
 
-module.exports = router;
+  module.exports = router;
