f249ed2869e28d2fb447ef7c26914876d26e2c7a
ZhiJun01 <147889637+ZhiJun01@users.noreply.github.com>
2024-09-03 17:05:48 +0800

    removal of map & searchbar in all pages

--- client/src/index.js
+++ client/src/index.js
@@ -42,7 +42,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <MapRoute />
+    //<MapRoute />
     <Footer />
   </React.StrictMode>
 );
