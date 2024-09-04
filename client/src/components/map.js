e13fa001ae4e54d0bb4b2f78d2c5826e695f3fa9
Sherman <shermantaymk@gmail.com>
2024-09-04 16:43:13 +0800

    fix route to be viewable on mobile

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -4,18 +4,16 @@ import React, { useState, useEffect } from "react";
 import { isEmpty } from "lodash";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import CardActionArea from "@mui/material/CardActionArea";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Collapse from "react-bootstrap/Collapse";
+import FlipComponent from "./FlipCardComponent";
+import MobileFlipComponent from "./MobileFlipCardComponent";
+import { useMediaQuery } from "react-responsive";
 
 function MapComponent({ result }) {
+  const isLaptop = useMediaQuery({ query: "(min-width: 1224px)" });
   const defaultUrl = "https://www.onemap.gov.sg/amm/amm.html?mapStyle=Default";
 
   const [url, setUrl] = useState("");
   const [openStates, setOpenStates] = useState([]); // Array to manage open state for each card
-
-  let count = 1;
 
   useEffect(() => {
     if (isEmpty(result)) {
@@ -32,14 +30,6 @@ function MapComponent({ result }) {
     }
   }, [result]);
 
-  const handleToggle = (index) => {
-    setOpenStates((prevOpenStates) => {
-      const newOpenStates = [...prevOpenStates];
-      newOpenStates[index] = !newOpenStates[index]; // Toggle the state for the specific index
-      return newOpenStates;
-    });
-  };
-
   return (
     <Container fluid>
       <Row className="g-4">
@@ -52,70 +42,21 @@ function MapComponent({ result }) {
             allowFullScreen="allowfullscreen"
           ></iframe>
         </Col>
-        <Col lg={6}>
-          {result.route?.length > 0 &&
-            result.route.map((item, index) => (
-              <React.Fragment key={index}>
-                {" "}
-                {/* Use index as a unique key */}
-                <p>Route {count++}</p>
-                <Row className="g-2">
-                  <Col>
-                    <Card>
-                      <CardActionArea
-                        onClick={() => handleToggle(index)} // Toggle specific card
-                        aria-controls={`route_details_${index}`}
-                        aria-expanded={openStates[index]}
-                      >
-                        <CardContent>
-                          <Row>
-                            <Col>
-                              <p>Fare: {item.fare}</p>
-                            </Col>
-                            <Col>
-                              <p>
-                                Duration: {parseInt(item.duration / 60)} mins
-                              </p>
-                            </Col>
-                            <Col>
-                              <p>Transfers: {item.transfers}</p>
-                            </Col>
-                          </Row>
-                          <Collapse in={openStates[index]}>
-                            <div id={`route_details_${index}`}>
-                              <Row>
-                                <Col>
-                                  <p>From</p>
-                                </Col>
-                                <Col>
-                                  <p>To</p>
-                                </Col>
-                                <Col>Via</Col>
-                                <Col>Bus No./ MRT</Col>
-                              </Row>
-                              {item.legs?.length > 0 &&
-                                item.legs.map((details, legIndex) => (
-                                  <Row key={legIndex}>
-                                    <Col>
-                                      <p>{details.from.name}</p>
-                                    </Col>
-                                    <Col>
-                                      <p>{details.to.name}</p>
-                                    </Col>
-                                    <Col>{details.mode}</Col>
-                                    <Col>{details.route}</Col>
-                                  </Row>
-                                ))}
-                            </div>
-                          </Collapse>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Col>
-                </Row>
-              </React.Fragment>
-            ))}
-        </Col>
+        {isLaptop ? (
+          <Col lg={6}>
+            {result.route?.length > 0 &&
+              result.route.map((item, index) => (
+                <FlipComponent item={item} index={index} key="" />
+              ))}
+          </Col>
+        ) : (
+          <Col lg={6}>
+            {result.route?.length > 0 &&
+              result.route.map((item, index) => (
+                <MobileFlipComponent item={item} index={index} key="" />
+              ))}
+          </Col>
+        )}
       </Row>
     </Container>
   );
