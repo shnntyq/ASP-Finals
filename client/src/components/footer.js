8c7314771e239eb90a7d8843f1b13041655d6f7f
ZhiJun01 <147889637+ZhiJun01@users.noreply.github.com>
2024-09-04 15:10:30 +0800

    Update footer.js

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/esm/Container";
 function Footer() {
   return (
     <Container className="fixed-bottom">
-      <footer className="text-center">
+      <footer className="text-end">
         <p>Copyright © 2024</p>
       </footer>
     </Container>
