961f804db18f2343a1ba2050f97772f2ca27c492
Sherman <shermantaymk@gmail.com>
2024-09-04 09:42:15 +0800

    fix all flashcard expanded when one is clicked

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -4,25 +4,12 @@ import React, { useState, useEffect } from "react";
 import { isEmpty } from "lodash";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import CardActionArea from "@mui/material/CardActionArea";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Collapse from "react-bootstrap/Collapse";
+import FlipComponent from "./FlipCardComponent";
 
 function MapComponent({ result }) {
   const defaultUrl = "https://www.onemap.gov.sg/amm/amm.html?mapStyle=Default";
 
   const [url, setUrl] = useState("");
-  const [open, setOpen] = useState(false);
-
-  let count = 1;
-
-  function convertUnixTime(unixTime) {
-    const dt = new Date(unixTime);
-    const currTime = dt.getHours() + ":" + dt.getMinutes();
-
-    return currTime;
-  }
 
   useEffect(() => {
     if (isEmpty(result)) {
@@ -47,67 +34,8 @@ function MapComponent({ result }) {
         </Col>
         <Col>
           {result.route?.length > 0 &&
-            result.route.map((item) => (
-              <>
-                <p>Route {count++}</p>
-                <Row>
-                  <Card>
-                    <CardActionArea
-                      onClick={() => setOpen(!open)}
-                      aria-controls="route_details"
-                      aria-expanded={open}
-                    >
-                      <CardContent>
-                        <Col>
-                          <p key="">Fare: {item.fare}</p>
-                        </Col>
-                        <Col>
-                          <p key="">
-                            Duration: {parseInt(item.duration / 60)} mins
-                          </p>
-                        </Col>
-                        <Col>
-                          <p key="">Transfers: {item.transfers}</p>
-                        </Col>
-                        <Collapse in={open}>
-                          <div id="route_details">
-                            <Row key="">
-                              <Col>
-                                <p>From</p>
-                              </Col>
-                              <Col>
-                                <p>To</p>
-                              </Col>
-                              <Col>Via</Col>
-                              <Col>Bus No./ MRT</Col>
-                              <Col>Time</Col>
-                            </Row>
-                            {item.legs?.length > 0 &&
-                              item.legs.map((details) => (
-                                <Row key="">
-                                  <Col>
-                                    <p>{details.from.name}</p>
-                                  </Col>
-                                  <Col>
-                                    <p>{details.to.name}</p>
-                                  </Col>
-                                  <Col>{details.mode}</Col>
-                                  <Col>{details.route}</Col>
-                                  <Col>
-                                    {details.route.length > 0 &&
-                                      convertUnixTime(
-                                        parseInt(details.from.arrival)
-                                      )}
-                                  </Col>
-                                </Row>
-                              ))}
-                          </div>
-                        </Collapse>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Row>
-              </>
+            result.route.map((item, index) => (
+              <FlipComponent item={item} index={index} key="" />
             ))}
         </Col>
       </Row>
